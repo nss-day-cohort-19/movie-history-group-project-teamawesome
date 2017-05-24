40218a03e44d39fd77b9af43392671e1d404a28c
Aarti Jaisinghani <jaisina@Aartis-MacBook-Air.local>
2017-05-24 10:53:42 -0500

    db interaction

---
+++ js/api-getter.js
@@ -0,0 +1,18 @@
+"use strict";
+
+function getKey() {
+  return {
+    apiKey: "AIzaSyCPKixA-kqlcaeT4eiKyqX2mcHpxvu95kM",
+    authDomain: "movie-history-2057a.firebaseapp.com",
+    databaseURL: "https://movie-history-2057a.firebaseio.com"
+  };
+}
+
+function getmovieDBKey() {
+  return {
+    apiKey: "08c884af213d59e7fc0438a466fac5ab",
+    databaseURL: ""
+  };
+}
+
+module.exports = {getKey, getmovieDBKey};
